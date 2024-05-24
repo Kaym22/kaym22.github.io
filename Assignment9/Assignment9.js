56e0761fcd5d9750019b0e0484c5369e083e2782
YouwishKendra <162417040+Kaym22@users.noreply.github.com>
2024-05-24 08:46:38 -0400

    Update Assignment9.js

--- Assignment9/Assignment9.js
+++ Assignment9/Assignment9.js
@@ -65,19 +65,7 @@ document.querySelector('#addToCartFormpear').addEventListener('submit', function
     }
 });
 
-var calculateTotalButtons = document.querySelectorAll('.calculateTotal');
 
-calculateTotalButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        var parentBox = button.closest('.subbox');
-        var quantityInput = parentBox.querySelector('.quantityInput');
-        var priceElement = parentBox.querySelector('.price');
-        var pricePerItem = parseFloat(priceElement.textContent.replace('$', ''));
-        var quantity = parseInt(quantityInput.value);
-        var totalPrice = pricePerItem * quantity; 
-        alert(' Your total price is: $' + totalPrice.toFixed(2));
-    });
-});
 
 
 
