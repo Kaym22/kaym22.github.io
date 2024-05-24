d4f3021f082aefe8c176c4ca5aca7699a6d0586d
YouwishKendra <162417040+Kaym22@users.noreply.github.com>
2024-05-24 08:52:07 -0400

    Update Assignment9.js

--- Assignment9/Assignment9.js
+++ Assignment9/Assignment9.js
@@ -13,56 +13,18 @@ function changeMainImage1(smallImage1) {
     smallImage1.src = oldSrc;
 }
 
-document.querySelector('#addToCartForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    var quantity = document.querySelector('#quantityInput').value;
-    
-    if (quantity !== '') {
-        var message;
-        if (quantity === '1') {
-            message = ' order is received!';
-        } else {
-            message = ' orders received!';
-        }
-        alert(quantity + ' ' + message);
-        document.querySelector('#quantityInput').value = '';
-    }
-});
+var calculateTotalButtons = document.querySelectorAll('.calculateTotal');
 
-
-document.querySelector('#addToCartFormapple').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    var quantity = document.querySelector('#quantityInput1').value;
-    
-    if (quantity !== '') {
-        var message;
-        if (quantity === '1') {
-            message = ' order is received!';
-        } else {
-            message = ' orders received!';
-        }
-        alert(quantity + ' ' + message);
-        document.querySelector('#quantityInput1').value = '';
-    }
-});
-
-document.querySelector('#addToCartFormpear').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    var quantity = document.querySelector('#quantityInput2').value;
-    
-    if (quantity !== '') {
-        var message;
-        if (quantity === '1') {
-            message = ' order is received!';
-        } else {
-            message = ' orders received!';
-        }
-        alert(quantity + ' ' + message);
-        document.querySelector('#quantityInput2').value = '';
-    }
+calculateTotalButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        var parentBox = button.closest('.subbox');
+        var quantityInput = parentBox.querySelector('.quantityInput');
+        var priceElement = parentBox.querySelector('.price');
+        var pricePerItem = parseFloat(priceElement.textContent.replace('$', ''));
+        var quantity = parseInt(quantityInput.value);
+        var totalPrice = pricePerItem * quantity; 
+        alert(' Your total price is: $' + totalPrice.toFixed(2));
+    });
 });
 
 
